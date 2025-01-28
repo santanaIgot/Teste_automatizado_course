3ccc16f08273b40e0dbf979d842d9b5396ad38c0
santanaIgot <santanaigoor01@gmail.com>
2025-01-28 15:04:37 -0300

    stubs login incorreto

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,20 +1,26 @@
-describe('Página de login', () => {
-    beforeEach(() => {
-        cy.visit('https://adopet-frontend-cypress.vercel.app/');
-        cy.get('[data-test="login-button"]').click();
+describe("Página de login", () => {
+  beforeEach(() => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app/");
+    cy.get('[data-test="login-button"]').click();
 
-        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
-            statusCode:400, }).as('stubPost')
-        })
+    cy.intercept(
+      "POST",
+      "https://adopet-api-i8qu.onrender.com/adotante/login",
+      {
+        statusCode: 400,
+      }
+    ).as("stubPost");
+  });
 
-    it('Deve preencher os campos do login incorretamente', () => {
-        cy.loginIncorreto('igot0@gmail.com','Senha423')
-      })
+  it("Deve preencher os campos do login incorretamente", () => {
+    cy.loginIncorreto("igot0@gmail.com", "Senha423");
+  });
 
-
-      it('Deve falhar mesmo que os campos sejam preenchidos corretamente', ()=> {
-        cy.login('igot02@gmail.com', 'Senha123')
-        cy.wait('@stubPost')
-        cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible')
-})
-})
+  it("Deve falhar mesmo que os campos sejam preenchidos corretamente", () => {
+    cy.login("igot02@gmail.com", "Senha123");
+    cy.wait("@stubPost");
+    cy.contains("Falha no login. Consulte suas credenciais.").should(
+      "be.visible"
+    );
+  });
+});
