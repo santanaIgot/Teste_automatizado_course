813bae8c187ee8caa4af2009ae34e589f0c2d4e7
santanaIgot <santanaigoor01@gmail.com>
2025-02-04 18:01:30 -0300

    flaky tests

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,8 +1,9 @@
 describe('api adopet', () => {
-    const authorization = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-    eyJzdWIiOiI1ZmZmNjQ0Mi05NjBkLTQyOTgtOTc2OC04Y2RmMGY0NmU1ZTkiLCJhZG9wdGVyT
-    mFtZSI6Iklnb3IiLCJpYXQiOjE3MzgyNTkwOTQsImV4cCI6MTczODUxODI5NH0
-    .QByrIhxko0GkxRD0DrtrTT44KESdi_KQRybuyOA460`
+    //Math.random é um método do javascript que vai ser multiplicado por 1000
+    const tempoEsperado = Math.random() * 1000
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZmZmNjQ0Mi05NjBkLTQyOTgtO
+    Tc2OC04Y2RmMGY0NmU1ZTkiLCJhZG9wdGVyTmFtZSI6Iklnb3IiLCJpYXQiOjE3Mzg2ODYxOTksImV4cCI6MTczODk0NTM5OX0
+    `
 
     it('mensagens da api', () => {
         cy.request({
@@ -14,6 +15,10 @@ describe('api adopet', () => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('msg')
+            //aqui ele vai consumir o tempo esperado, que é o método que armazenamos na variável acima
+            //lte significa menor que 
+            //nesta linda de código vamos espera que esse tempo seja inferior(lte) a nossa variável 'tempoEsperado'
+            expect(res.duration).to.be.lte(tempoEsperado)
         })
     })
 })
